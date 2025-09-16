9f6f27acc56e81b8c1a9f4505b85e3c816c413a2
minarae <minarae@gmail.com>
2025-09-16 22:37:56 +0900

    Next.js 설정 업데이트: React 엄격 모드 활성화, Webpack 외부 모듈에 'kakao' 추가, 이미지 최적화 비활성화 설정을 추가하여 성능 및 사용자 경험을 개선하였습니다.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+    webpack: (config) => {
+        config.externals = [...config.externals, 'kakao'];
+        return config;
+    },
+    images: {
+        unoptimized: true, // 이미지 최적화 활성화
+    },
 };
 
 export default nextConfig;
