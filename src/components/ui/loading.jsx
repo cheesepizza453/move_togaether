981fd01feed089eb53f6a4262e6a16b78c5c74ab
cheesepizza453 <earlykim1209@gmail.com>
2025-11-21 12:05:45 +0900

    [modify] 로딩 스타일 수정

--- src/components/ui/loading.jsx
+++ src/components/ui/loading.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Loading = ({text, className}) => {
     return (
         <div className="min-h-screen bg-white flex justify-center items-center">
-            <div className={'w-full flex justify-center translate-y-[-50%]'}>
+            <div className={'w-full flex justify-center translate-y-[-50%] mt-[-86px]'}>
                 <IconLoading text={text} className={className}/>
             </div>
         </div>
