eac135d4a2618633ef17fc193fee0f056af0ee01
cheesepizza453 <earlykim1209@gmail.com>
2026-06-17 12:04:50 +0900

    [fix] 아이폰 스타일

--- src/components/volunteer/Step1.jsx
+++ src/components/volunteer/Step1.jsx
@@ -58,7 +58,7 @@ const Step1 = ({
           maxLength={40}
           onChange={(e) => onFormDataChange('title', e.target.value)}
           placeholder="제목을 입력해주세요"
-          className={`w-full h-[52px] px-[18px] border rounded-[15px] text-text-800 focus:text-brand-yellow-dark focus:bg-brand-sub focus:outline-none focus:ring-1 focus:ring-[#FFD044] focus:border-transparent transition-colors ${
+          className={`appearance-none w-full h-[52px] px-[18px] border rounded-[15px] text-text-800 focus:text-brand-yellow-dark focus:bg-brand-sub focus:outline-none focus:ring-1 focus:ring-[#FFD044] focus:border-transparent transition-colors ${
             errors.title ? 'border-red-500' : 'border-gray-300'
           }`}
         />
